f492fd16839463073ab9805352a14d995d106bd4
Paul <paumallet76@gmail.com>
2023-10-19 20:03:56 +0200

    Added Islands with Preact and Astro directives

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://paul-blog-with-astro.netlify.app"
+  site: "https://paul-blog-with-astro.netlify.app",
+  integrations: [preact()]
 });
